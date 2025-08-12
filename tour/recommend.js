7b9bda9b1e1a197454ab4dde8ee98f35e4532e51
wagura Maurice <buisness@waguramaurice.com>
2025-08-12 23:59:21 +0300

    update recommend.js

--- tour/recommend.js
+++ tour/recommend.js
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 element: '.logo-header',
                 on: 'bottom'
             },
-            scrollTo: { behavior: 'smooth', block: 'center' },,
+            scrollTo: { behavior: 'smooth', block: 'center' },
             buttons: [
                 {
                     text: 'Next',
